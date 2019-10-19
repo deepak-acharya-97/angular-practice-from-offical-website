ccbbb77bc5c90ad8a7d28e1ce063d8d689107068
deepak-acharya-97 <aacharyadeepak100@gmail.com>
2019-10-19 22:25:42 +0530

    Design shipping view; async instead of subscribe

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,11 +7,11 @@ import { CartService } from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
-
+  shippingCosts;
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
-    
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
